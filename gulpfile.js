69e4a25445a060d159fb8df7bf080f4b4182930a
EXCELINDIA\sharath.r <sharath.r@excelindia.com>
2023-07-04 12:52:16 +0530

    Task Added

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 var gulp = require("gulp");
-var msbuild = require("gulp-msbuild");
 const Logger = require('fancy-log');
 
 
-gulp.task("ApplicationBuild", function() {
-	return gulp.src('D:\build\code\MVCCoreDemo.csproj' )
-            .pipe(msbuild({
-				toolsVersion: '17.5.1',
-                targets: ['Clean', 'Build'], //Always do a CLEAN BUILD
-                properties: { Configuration: 'Release' }
-				
-                })
-            )
-            .on('end', function() {Logger.info(LogMessage='build successfull'); })
-            .on('error', function() {Logger.error(LogMessage='build failed');});
+
+require('./task/buildproject');
+
+
+gulp.task("ApplicationBuild", gulp.series('ApplClean','ApplBuild'), function(done) {
+  Logger.info(LogMessage='build successfull');
+  done();
 });	
+
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
---
+++ task/buildproject.js
@@ -0,0 +1,39 @@
+var gulp = require("gulp");
+var msbuild = require("gulp-msbuild");
+const Logger = require('fancy-log');
+
+
+gulp.task("ApplClean", function() {
+	return gulp.src('D:\build\code\MVCCoreDemo.csproj' )
+            .pipe(msbuild({
+				toolsVersion: '4.0.30319',
+                targets: ['Clean'], //Always do a CLEAN BUILD
+                properties: { Configuration: 'Release' }
+				
+                })
+            )
+            .on('end', function() {Logger.info(LogMessage='clean successfull'); })
+            .on('error', function() {Logger.error(LogMessage='failed');});
+});	
+
+gulp.task("ApplBuild", function() {
+	return gulp.src('D:\build\code\MVCCoreDemo.csproj' )
+            .pipe(msbuild({
+				toolsVersion: '4.0.30319',
+                targets: ['Build'], //Always do a CLEAN BUILD
+                properties: { Configuration: 'Release' }
+				
+                })
+            )
+            .on('end', function() {Logger.info(LogMessage='build successfull'); })
+            .on('error', function() {Logger.error(LogMessage='failed');});
+});	
+
+
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
